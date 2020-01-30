c3e2d56eaf04f0c7a10e94c4840043b2f458c983
kenji-kondo <feyn.miya@gmail.com>
2020-01-30 09:11:15 +0900

    add server.py

---
+++ server.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+from linear_regression import LinearRegression
+from make_sample import MakeSample
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def return_hello():
+    return render_template('result.html')
+
+
+m = MakeSample()
+m.make_sample()
+l = LinearRegression()
+l.work()
+
+app.run(port=8080, debug=True)
